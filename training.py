4f5c4fad6a2913646f2be4cd4599bffeaff46cb1
mdhvince <vinceslas.medhy@gmail.com>
2023-02-10 19:13:33 +0100

    allow notebook

--- training.py
+++ training.py
@@ -10,7 +10,6 @@ from load_data import *
 
 
 def train(n_epochs, train_loader, valid_loader, save_location_path, embed_size, hidden_size, vocab_size):
-
     encoder = EncoderCNN(embed_size)
     decoder = DecoderRNN(embed_size, hidden_size, vocab_size)
 
@@ -27,12 +26,12 @@ def train(n_epochs, train_loader, valid_loader, save_location_path, embed_size, 
     # Save the model according to this comparison
     valid_loss_min = np.Inf
 
-    for epoch in range(1, n_epochs+1):
-        
+    for epoch in range(1, n_epochs + 1):
+
         # Keep track of training and validation loss
         train_loss = 0.0
         valid_loss = 0.0
-        
+
         encoder.train()
         decoder.train()
         for data in train_loader:
@@ -40,7 +39,7 @@ def train(n_epochs, train_loader, valid_loader, save_location_path, embed_size, 
             images = images.type(torch.FloatTensor)
             images.to(device)
             captions.to(device)
-            
+
             decoder.zero_grad()
             encoder.zero_grad()
 
@@ -48,12 +47,11 @@ def train(n_epochs, train_loader, valid_loader, save_location_path, embed_size, 
             outputs = decoder(features, captions)
 
             loss = criterion(outputs.contiguous().view(-1, vocab_size), captions.view(-1))
-            loss.backward()  
+            loss.backward()
             optimizer.step()
-            
-            train_loss += loss.item()*images.size(0)
-        
-        
+
+            train_loss += loss.item() * images.size(0)
+
         encoder.eval()
         decoder.eval()
         for data in valid_loader:
@@ -61,30 +59,29 @@ def train(n_epochs, train_loader, valid_loader, save_location_path, embed_size, 
             images = images.type(torch.FloatTensor)
             images.to(device)
             captions.to(device)
-            
+
             features = encoder(images)
             outputs = decoder(features, captions)
 
             loss = criterion(outputs.contiguous().view(-1, vocab_size), captions.view(-1))
-            
-            valid_loss += loss.item()*images.size(0)
-            
+
+            valid_loss += loss.item() * images.size(0)
+
             # Average losses
-            train_loss = train_loss/len(train_loader)
-            valid_loss = valid_loss/len(valid_loader)
-            
+            train_loss = train_loss / len(train_loader)
+            valid_loss = valid_loss / len(valid_loader)
+
             print(f"Epoch: {epoch} \tTraining Loss: {train_loss} \tValidation Loss: {valid_loss}")
 
             # save model if validation loss has decreased
             if valid_loss <= valid_loss_min:
                 print(f"Validation loss decreased ({valid_loss_min} --> {valid_loss}).  Saving model ...")
-                torch.save(encoder.state_dict(), save_location_path+'/encoder{n_epochs}.pt')
-                torch.save(decoder.state_dict(), save_location_path+'/decoder{n_epochs}.pt')
+                torch.save(encoder.state_dict(), save_location_path + '/encoder{n_epochs}.pt')
+                torch.save(decoder.state_dict(), save_location_path + '/decoder{n_epochs}.pt')
                 valid_loss_min = valid_loss
 
 
 if __name__ == '__main__':
-
     csv_file = 'data/results2.csv'
     root_dir = 'data/flickr30k_images'
     mapper_file = 'mapping.pkl'
@@ -94,8 +91,6 @@ if __name__ == '__main__':
     valid_size = 0.3
     embed_size = 256
     hidden_size = 512
-    
-    
 
     train_set, vocab = create_dataset(csv_file, root_dir, mapper_file)
     train_sampler, valid_sampler = train_valid_split(train_set, valid_size)
